5d372a8e5d2312add4ea746f2f52d3ef157926e5
Skylarking212 <oliverwtaylor1@gmail.com>
2023-08-15 07:57:06 -0400

    Create a dex
     Created a cool little dex

--- dex/src/index.js
+++ dex/src/index.js
@@ -3,12 +3,60 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { configureChains, mainnet, WagmiConfig, createClient } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+
+const https = require("https");
+const projectId = "<API-KEY>";
+const data = JSON.stringify({
+  jsonrpc: "2.0",
+  method: "eth_blockNumber",
+  params: [],
+  id: 1,
+});
+const options = {
+  host: "mainnet.infura.io",
+  port: 443,
+  path: "/v3/" + projectId,
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+const req = https.request(options, (res) => {
+  console.log(`statusCode: ${res.statusCode}`);
+
+  res.on("data", (d) => {
+    process.stdout.write(d);
+  });
+});
+
+req.on("error", (error) => {
+  console.error(error);
+});
+
+req.write(data);
+req.end();
+
+const { provider, webSocketProvider } = configureChains(
+[mainnet],
+[publicProvider()]
+);
+
+const client = createClient({
+  autoConnect: true,
+  provider,
+  webSocketProvider,
+});
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <WagmiConfig client={client}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
+      </WagmiConfig>
   </React.StrictMode>
 );
